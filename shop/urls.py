b89310d124585c2f6e5535bacad449a6164c97cf
pranitastudent <pranita_gandhi@hotmail.com>
2019-12-16 15:19:06 +0000

    Accounts urls added - login. register, password reset

--- shop/urls.py
+++ shop/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('home.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
    
 ]
