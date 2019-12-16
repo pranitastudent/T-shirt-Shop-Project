0749aa8d86301bdd38b4ed285ebced068b4a2ef6
pranitastudent <pranita_gandhi@hotmail.com>
2019-12-16 12:50:22 +0000

    Google map and shop opening times added

--- shop/static/js/main.js
+++ shop/static/js/main.js
@@ -1,3 +1,18 @@
+
+// Year
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
 
+// Map
+
+function initMap() {
+    // Your location
+    const loc = { lat: 51.46759, lng: -0.3618 };
+    // Centered map on location
+    const map = new google.maps.Map(document.querySelector(".map"), {
+        zoom: 15,
+        center: loc
+    });
+    // The marker, positioned at location
+    const marker = new google.maps.Marker({ position: loc, map: map });
+}
