89f2fd74c86b4f3b02aebae8102aaf96f34e1a66
pranitastudent <pranita_gandhi@hotmail.com>
2019-12-17 14:16:59 +0000

    Registration still not working

--- accounts/views.py
+++ accounts/views.py
@@ -1,81 +1,61 @@
-from django.shortcuts import render, redirect,get_object_or_404, reverse
-from django.utils import timezone
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
-from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required 
-from .forms import UserLoginForm,  UserRegistrationForm
-from django.core.mail import EmailMessage
-from django.template.loader import get_template
+from django.contrib.auth.decorators import login_required
+from .forms import UserLoginForm, UserRegistrationForm
+
+# Logout View
+
+@login_required
+def logout(request):
+    """Log the user out"""
+    auth.logout(request)
+    messages.success(request, "You have successfully been logged out")
+    return redirect(reverse('index'))
+
+# Login View
+
+def login(request):
+    """Return a login page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                    password=request.POST['password'])
+            messages.success(request, "You have successfully logged in!")
+
+            if user:
+                auth.login(user=user, request=request)
+                return redirect(reverse('index'))
+            else:
+                login_form.add_error(None, "Your username or password is incorrect")
+    else:
+        login_form = UserLoginForm()
+    return render(request, 'accounts/login.html', {'login_form': login_form})
 
 # Registration View
 
 def register(request):
-    """Lets users register to site"""
+    """Render the registration page"""
     if request.user.is_authenticated:
-        messages.success(request, "You are already logged in", 
-                         extra_tags="alert-primary")
         return redirect(reverse('index'))
-        
+
     if request.method == "POST":
         registration_form = UserRegistrationForm(request.POST)
-        
+
         if registration_form.is_valid():
             registration_form.save()
-            
+
             user = auth.authenticate(username=request.POST['username'],
-            password=request.POST['password1'])
-            
+                                     password=request.POST['password1'])
             if user:
                 auth.login(user=user, request=request)
-                messages.success(request, "You have successfully registered!", 
-                                 extra_tags="alert-primary")
-                return redirect(reverse('index'))
+                messages.success(request, "You have successfully registered")
             else:
-                messages.error(request, 
-                               "Unable to register your account at this time!",
-                               extra_tags="alert-danger")
-
+                messages.error(request, "Unable to register your account at this time")
     else:
         registration_form = UserRegistrationForm()
     return render(request, 'accounts/register.html', {
-                  'registration_form': registration_form})
-    
-# Login View
-
-def login(request):
-    """Logs the user in"""
-    if request.user.is_authenticated:
-        messages.success(request, "You are already logged in!", 
-                         extra_tags="alert-primary")
-        return redirect(reverse('index'))
-        
-    if request.method == "POST":
-        login_form = UserLoginForm(request.POST)
-        if login_form.is_valid():
-            user = auth.authenticate(username=request.POST['username'],
-            password = request.POST['password'])
-            
-            if user:
-                auth.login(user=user, request=request)
-                messages.success(request, "You have successfully logged in!", 
-                                 extra_tags="alert-primary")
-                if request.GET.get('next', False):
-                    return HttpResponseRedirect(request.GET.get('next'))
-                else:
-                    return redirect(reverse('index'))
-            else:
-                login_form.add_error(None,
-                "Your username or password is incorrect!")
-    else:
-        login_form = UserLoginForm()
-    return render(request, 'accounts/login.html', {"login_form": login_form})
-
-# Logout View
-
-def logout(request):
-    """Log out the user"""
-    auth.logout(request)
-    messages.success(request, "You have successfully been logged out!", 
-                     extra_tags="alert-primary")
-    return redirect(reverse('index'))    
+        "registration_form": registration_form})
