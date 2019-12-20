6d0cb0683e837c057eb7386a39c35c38024134b8
pranitastudent <pranita_gandhi@hotmail.com>
2019-12-20 08:42:08 +0000

    Contact form view added

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response 
+from django.core.mail import EmailMessage
+from django.template.loader import get_template
+from .forms import ContactForm
 
-# Create your views here.
+# Contact Form- Code adapted from (Django-2.2 Part-7 Django Contact Form with SMTP Email Backed Tutorial | By Creative web) 
+
+def contact(request):
+    Contact_Form = ContactForm
+    if request.method == 'POST':
+        form = Contact_Form(data=request.POST) 
+        
+        if form.is_valid():
+            contact_name = request.POST.get('contact_name')
+            contact_email = request.POST.get('contact_email')
+            contact_content = request.POST.get('content')
+
+            template = get_template('contact_form.txt')
+            context = {
+                'contact_name' : contact_name,
+                'contact_email' : contact_email,
+                'contact_content' : contact_content,
+            }
+            
+            content = template.render(context)
+        
+        email = EmailMessage(
+            "New contact form email",
+            content,
+            "Pranita's T-shirt Shop" + '',
+            ['pranitacoder12@gmail.com'],
+            headers = { 'Reply To': contact_email }
+            
+        )
+            
+        email.send()
+            
+        return render_to_response('success.html')
+    return render(request, 'contact.html', {'form':Contact_Form })   
+    
+
