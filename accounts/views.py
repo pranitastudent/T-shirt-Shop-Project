9e2fd9abe3bb4f3b43e3d89cd3088a19b431e876
pranitastudent <pranita_gandhi@hotmail.com>
2019-12-20 07:47:04 +0000

    Login via email does not work

--- accounts/views.py
+++ accounts/views.py
@@ -10,7 +10,7 @@ def logout(request):
     """Log the user out"""
     auth.logout(request)
     messages.success(request, "You have successfully been logged out")
-    return redirect(reverse('index'))
+    return redirect(reverse('logout'))
 
 # Login View
 
