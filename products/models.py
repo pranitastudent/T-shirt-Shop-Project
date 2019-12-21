7148e1070412d90b594749a04fca765b20a8f37c
pranitastudent <pranita_gandhi@hotmail.com>
2019-12-21 12:34:54 +0000

    Product model not looping through products

--- products/models.py
+++ products/models.py
@@ -2,25 +2,18 @@ from django.db import models
 
 # Create your models here.
 
-SIZE_CHOICES = (
-    ('S', 'Small'),
-    ('M', 'Medium'),
-    ('L', 'Large'),
-    ('XL', 'XLarge')
-)
-
-COLOR_CHOICES = (
-    ('Red', 'Red'),
-    ('Green', 'Green'),
-    ('Black', 'Black'),
-    ('White', 'White')
+GENDER_CHOICES = (
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+    ('Girl', 'Girl'),
+    ('Boy', 'Boy')
 )
 class Product(models.Model):
     product_name = models.CharField(max_length=254)
     price = models.DecimalField(max_digits=3,decimal_places=2)
     image = models.ImageField(upload_to='images')
-    category = models.CharField(choices=SIZE_CHOICES, max_length=1)
-    size = models.CharField(choices=COLOR_CHOICES, max_length=1)
+    category = models.TextField(choices=GENDER_CHOICES)
+   
     
     
     
