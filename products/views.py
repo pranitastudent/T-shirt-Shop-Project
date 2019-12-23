6d948c89d0bcab10db29d68f52b1eeb31fa8997f
pranitastudent <pranita_gandhi@hotmail.com>
2019-12-23 11:04:58 +0000

    Pagination complete and working and styles added to background and li elements

--- products/views.py
+++ products/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator,EmptyPage, PageNotAnInteger
 from .models import Product
 
 # Product View
 
 def all_products(request):
     products = Product.objects.all()
-    return render(request,"products/products.html", {"products":products})
+    paginator = Paginator(products, 6)
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
+    return render(request,"products/products.html", {"products":paged_products})
