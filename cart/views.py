25d8f8648ab384e0b26ee9ceb55fe0cf7ab7cc53
pranitastudent <pranita_gandhi@hotmail.com>
2019-12-25 16:03:09 +0000

    Cart views.py work for pop - if quantity < 1

--- cart/views.py
+++ cart/views.py
@@ -1,39 +1,38 @@
 from django.shortcuts import render, redirect, reverse
 
-# Code adapted from Code Institute
-
-# View everything in cart
-
+# Create your views here.
 def view_cart(request):
+    """A View that renders the cart contents page"""
     return render(request, "cart/cart.html")
-
-# Add to cart
 
 
 def add_to_cart(request, id):
-    
-    quantity = int(request.POST.get('quantity'))
-
+    """Add a quantity of the specified product to the cart"""
+   
+    quantity = 1
     cart = request.session.get('cart', {})
     if id in cart:
+        print(cart)
         cart[id] = int(cart[id]) + quantity      
     else:
         cart[id] = cart.get(id, quantity) 
 
     request.session['cart'] = cart
-    return redirect(reverse('index'))
+    return redirect(reverse('view_cart'))
 
-# Adjust quantity against price
 
 def adjust_cart(request, id):
-    
+    """
+    Adjust the quantity of the specified product to the specified
+    amount
+    """
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[id] = quantity
     else:
-        cart.pop(id)
+        cart.pop(str(id))
     
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
