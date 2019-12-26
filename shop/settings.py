af9ec082046970331dd2495a8d156e6899caa130
pranitastudent <pranita_gandhi@hotmail.com>
2019-12-26 11:58:03 +0000

    Python manage.py migrate not working in new postgresql database

--- shop/settings.py
+++ shop/settings.py
@@ -14,6 +14,8 @@ import os
 
 if os.path.exists('env.py'):
     import env
+    
+import dj_database_url    
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -108,12 +110,14 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
