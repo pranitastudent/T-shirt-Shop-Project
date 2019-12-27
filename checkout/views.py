0b255da7470d5458e1af3292e4de3b5b59e1f88d
pranitastudent <pranita_gandhi@hotmail.com>
2019-12-27 15:10:37 +0000

    Stripe not working- stripe id error

--- checkout/views.py
+++ checkout/views.py
@@ -11,44 +11,45 @@ from products.models import Product
 import stripe
 
 
-stripe.api_key = settings.STRIPE_SECRET
+# Create your views here.
 
+stripe.api_key = settings.STRIPE_SECRET
 
 @login_required()
 def checkout(request):
-    if request.method == "POST":
+    if request.method=="POST":
         order_form = OrderForm(request.POST)
         payment_form = MakePaymentForm(request.POST)
-
+        
         if order_form.is_valid() and payment_form.is_valid():
             order = order_form.save(commit=False)
             order.date = timezone.now()
             order.save()
-
+            
             cart = request.session.get('cart', {})
             total = 0
             for id, quantity in cart.items():
                 product = get_object_or_404(Product, pk=id)
-                total += quantity * product.price       
+                total += quantity * product.price
                 order_line_item = OrderLineItem(
-                    order=order,
-                    product=product,
-                    quantity=quantity
-                )
+                    order = order, 
+                    product = product, 
+                    quantity = quantity
+                    )
                 order_line_item.save()
-            
+                
             try:
                 customer = stripe.Charge.create(
-                    amount=int(total * 100),
-                    currency="EUR",
-                    description=request.user.email,
-                    card=payment_form.cleaned_data['stripe_id'],
+                    amount = int(total * 100),
+                    currency = "GBP",
+                    description = request.user.email,
+                    card = payment_form.cleaned_data['stripe_id'],
                 )
             except stripe.error.CardError:
                 messages.error(request, "Your card was declined!")
-            
+                
             if customer.paid:
-                messages.success(request, "You have successfully paid")
+                messages.error(request, "You have successfully paid")
                 request.session['cart'] = {}
                 return redirect(reverse('products'))
             else:
@@ -59,5 +60,5 @@ def checkout(request):
     else:
         payment_form = MakePaymentForm()
         order_form = OrderForm()
-    
-    return render(request, "checkout/checkout.html", {"order_form": order_form, "payment_form": payment_form, "publishable": settings.STRIPE_PUBLISHABLE})
+        
+    return render(request, "checkout/checkout.html", {'order_form': order_form, 'payment_form': payment_form, 'publishable': settings.STRIPE_PUBLISHABLE})
