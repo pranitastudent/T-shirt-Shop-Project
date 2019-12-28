fc87057ba4759a83325276b36282713ea0724487
pranitastudent <pranita_gandhi@hotmail.com>
2019-12-28 12:09:58 +0000

    Urls - feedback - get, edit, delete and detail complete - now need to add views to match

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from .views import all_products
 from django.conf.urls import url
-from . import views
+from .views import (get_feedback, add_feedback, edit_feedback, delete_feedback, detail_feedback)
 
 urlpatterns = [
-    path('products/', all_products, name='products'), 
-    path('search', views.do_search, name = 'search'),
+    path('get_feedback/', get_feedback, name='get_feedback'), 
+    path('<int:pk>/add_feedback', add_feedback, name= 'add_feedback'),
+    path('<int:pk>/edit_feedback', edit_feedback, name='edit_feedback'),
+    path('<int:pk>/delete_feedback', delete_feedback, name='delete_feedback'),
+    path('<int:pk>/detail_feedback', detail_feedback, name='detail_feedback'),
+        
    
       
 ]
