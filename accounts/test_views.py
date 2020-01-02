71e3cf9e4fe5288891c23949600c22c7f385bcb6
pranitastudent <pranita_gandhi@hotmail.com>
2020-01-02 16:00:58 +0000

    View tests only run on sqlitedb3

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -2,11 +2,33 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
 from accounts.forms import UserRegistrationForm, UserLoginForm
+from home.views import index
 
 class TestAccountsViews(TestCase):
     
     def test_registration_page(self):
         response = self.client.get('/accounts/register/')
         self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'templates/accounts/register.html')
+        self.assertTemplateUsed(response, 'accounts/register.html')
     
+    def test_login_page(self):
+        response = self.client.get('/accounts/login/')
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/login.html')
+        
+    def test_logged_in_page(self):
+        user = User.objects.create_user(
+            'test_user',
+            'test_user@gmail.com'
+            'example'
+        )
+        user.save()
+        logged_in = self.client.login(username='test_user', password='example')
+        response = self.client.get('/home/index/')
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')    
+    
+   
+            
+        
+        
