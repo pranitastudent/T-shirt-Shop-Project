eecb6380b10b5024b61025a4a16d84cf84348939
pranitastudent <pranita_gandhi@hotmail.com>
2020-01-02 14:09:40 +0000

    Urls testing complete for cart, checkout, feedback and contact apps

---
+++ cart/test_urls.py
@@ -0,0 +1,18 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from cart.views import view_cart
+
+
+class TestUrls(SimpleTestCase):
+    
+    # View Cart
+    
+    def test_view_cart_url_resolves(self):
+      url = reverse('view_cart')
+      print(resolve(url))
+      self.assertEquals(resolve(url).func,view_cart)
+      
+   
+   
+    
+      
