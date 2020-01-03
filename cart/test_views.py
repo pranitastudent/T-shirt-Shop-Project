def7b6987a3d34f1d987e3c463bbde02e7758390
pranitastudent <pranita_gandhi@hotmail.com>
2020-01-03 11:23:30 +0000

    Test views failing for checkout and cart

--- cart/test_views.py
+++ cart/test_views.py
@@ -1,2 +1,13 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
 
+
+
+class TestCartView(TestCase):
+    
+    def test_add_to_cart_page(self):
+        response = self.client.get('/cart/add_to_cart/')
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'checkout/checkout.html')
     
