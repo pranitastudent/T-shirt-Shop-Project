3b9eaddbbc6fc6661f2f824c50a25f5ebeafcae9
pranitastudent <pranita_gandhi@hotmail.com>
2020-01-06 12:34:34 +0000

    CSS stylesheet validated using W3C CSS Validation Service

--- shop/settings.py
+++ shop/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY =  os.environ.get('SECRET_KEY')
 GOOGLE_RECAPTCHA_SECRET_KEY = os.environ.get('GOOGLE_RECAPTCHA_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
