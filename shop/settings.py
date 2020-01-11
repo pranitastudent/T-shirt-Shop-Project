9c07f8f53c47f5f4d79ee006c042d2311bdd140f
pranitastudent <pranita_gandhi@hotmail.com>
2020-01-11 11:56:27 +0000

    Email host changed back to smtp.gmail.com

--- shop/settings.py
+++ shop/settings.py
@@ -194,7 +194,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 #  Testing - whether real email is sent
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 EMAIL_PORT = 587
