336833abceb5f522dee7fa3b904caf1a7394ccb6
pranitastudent <pranita_gandhi@hotmail.com>
2020-01-11 11:37:08 +0000

    SMTP sender refused connection error persists on Heroku, TLS set to False

--- shop/settings.py
+++ shop/settings.py
@@ -193,11 +193,12 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 #  Testing - whether real email is sent
 
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 EMAIL_PORT = 587
+
 
 
 # STRIPE KEYS
